31998c3be8c5ddaec70d3981990d8b3ca7100e3d
marvisa22 <107163520+marvisa22@users.noreply.github.com>
2024-11-27 17:35:07 -0500

    Menu Pausa
    Se crea el menu pausa para pausar con boton en el nivel y con la tecla escape

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Jugar();
+            }
+            else 
+            {
+                Pausa();
+            }
+        }
+    }
+    public void Pausa()
+    {
+        juegoPausado=true;
+        Time.timeScale = 0f ;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Jugar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Restaurar()
+    {
+        Time.timeScale = 0f ;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Cerrando el juego");
+        Application.Quit();
+    }
+}
