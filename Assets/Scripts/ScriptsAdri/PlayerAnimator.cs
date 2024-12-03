3f92a50641915801c593625809869e11ec290e3a
marvisa22 <107163520+marvisa22@users.noreply.github.com>
2024-12-03 10:35:42 -0500

    Prefabs Pistas
    Se crean las 5 pistas fisicas
    Reloj
    Carta
    Recibo
    Mapa
    Piedra
    
    Me falta darle diseño al inventario y aplicar la funcionalidad que cuando el jugador de clic en un elemento del inventario este haga zoom

--- Assets/Scripts/ScriptsAdri/PlayerAnimator.cs
+++ Assets/Scripts/ScriptsAdri/PlayerAnimator.cs
@@ -9,29 +9,58 @@ public class PlayerAnimator : MonoBehaviour
     public float speedRotate = 200.0f;
     private Animator animator;
     public float x, y;
-   
+    public Rigidbody rb;
+    public float fuerzaDeSalto = 8f;
+    public bool puedoSaltar;
+
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>(); 
+        puedoSaltar = false;
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speedMovement * Time.deltaTime * y);
+        transform.Rotate(Vector3.up * speedRotate * Time.deltaTime * x);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       /* x = Input.GetAxis("Horizontal");
+        /* x = Input.GetAxis("Horizontal");
+         y = Input.GetAxis("Vertical");
+
+         transform.Rotate(0, x * Time.deltaTime * speedRotate, 0);
+         transform.Translate(0, 0, y * Time.deltaTime * speedMovement, 0);*/
+
         y = Input.GetAxis("Vertical");
-
-        transform.Rotate(0, x * Time.deltaTime * speedRotate, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * speedMovement, 0);*/
-
-        y = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * speedMovement * Time.deltaTime * y);
-
         x = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * speedRotate * Time.deltaTime * x);
 
         animator.SetFloat("SpeedX", x);
         animator.SetFloat("SpeedY", y);
+
+        if (puedoSaltar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("Saltar", true);
+                rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse);
+            }
+            animator.SetBool("TocaSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+
+        }
+
+    }
+    public void EstoyCayendo()
+    {
+        animator.SetBool("TocaSuelo", true);
+        animator.SetBool("Saltar", true);
     }
 }
